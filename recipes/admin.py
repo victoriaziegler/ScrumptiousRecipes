e96dde2764c57f0bc13b918fb0647f6a91567488
Victoria Ziegler <toriziegler@cox.net>
2022-06-02 14:40:26 -0700

    first commit

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,31 @@
 from django.contrib import admin
-
+from recipes.models import Recipe, Step, Measure, FoodItem, Ingredient
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Step, StepAdmin)
+admin.site.register(Measure, MeasureAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,58 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " by " + self.author
+
+
+class Step(models.Model):
+    order = models.PositiveSmallIntegerField()
+    directions = models.TextField(max_length=300)
+    recipe = models.ForeignKey(
+        "Recipe", related_name="steps", on_delete=models.CASCADE
+    )
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.abbreviation
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+
+    recipe = models.ForeignKey(
+        "Recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+    measure = models.ForeignKey("Measure", on_delete=models.PROTECT)
+    food = models.ForeignKey("FoodItem", on_delete=models.PROTECT)
+
+    def __str__(self):
+        amount_str = str(self.amount)
+        return (
+            amount_str
+            + " "
+            + self.measure.abbreviation
+            + " of "
+            + self.food.name
+        )
