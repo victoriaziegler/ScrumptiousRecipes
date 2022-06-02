9eca6b26fdce472837888c30829e6ff585b0cd1d
Victoria Ziegler <toriziegler@cox.net>
2022-06-02 15:36:26 -0700

    added tags

--- tags/models.py
+++ tags/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    recipes = models.ManyToManyField("recipes.Recipe", related_name="tags")
+
+    def __str__(self):
+        return self.name
