0e32dc686e734497bc859b60b8f61f883e0a2849
Victoria Ziegler <toriziegler@cox.net>
2022-09-28 15:39:05 -0700

    list view page functioning

--- recipes/models.py
+++ recipes/models.py
@@ -19,7 +19,7 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name + " by " + self.author
+        return str(self.name) + " by " + str(self.author)
 
 
 class Step(models.Model):
